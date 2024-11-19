849ec8b624745a4918e1f9ecbc29fe4ca97bb97c
vyvy <vyvy292004@gmail.com>
2024-11-18 22:42:43 -0300

    fix(database.py): Fixing cyclical import

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from database import db
+from models.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
+db.init_app(app)
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
