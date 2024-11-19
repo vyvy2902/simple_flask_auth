db6a4e51f07d40a0faccfca564f9999ddcfd5c9c
vyvy <vyvy292004@gmail.com>
2024-11-19 03:49:56 -0300

    feat(app.py): Implementation of user CRUD

--- app.py
+++ app.py
@@ -33,7 +33,50 @@ def login():
 def logout():
     logout_user()
     return jsonify({'message': 'Logout successfully'})
-    
 
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        user = User(username = username, password = password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'message': 'User successfully registered'})
+    
+    return jsonify({'message': 'Invalid data'}), 400
+
+@app.route('/user/<int:id_user>', methods = ['GET'])
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
+    if user:
+        return jsonify({'username': f'{user.username}'})
+    return jsonify({'message': 'User not found'}), 404
+
+@app.route('/user/<int:id_user>', methods = ['PUT'])
+@login_required
+def update_user(id_user):
+    user = User.query.get(id_user)
+    if user:
+        data = request.json
+        user.password = data.get('password')
+        db.session.commit()
+        return jsonify({'message': f'{user.username} updated successfully'})
+    return jsonify({'message': 'User not found'}), 404
+
+@app.route('/user/<int:id_user>', methods=['DELETE'])
+@login_required
+def delte_user(id_user):
+    user = User.query.get(id_user)
+    if user.id == current_user.id:
+        return jsonify({'message': 'Unauthorized deletion'}), 403
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': f'{user.username} deleted successfully'})
+    return jsonify({'message': 'User not found'}), 404
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
