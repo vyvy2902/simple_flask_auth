99117025c5f5a35e975fa5ad5ed8a06615d21bc8
vyvy <vyvy292004@gmail.com>
2024-11-19 04:10:22 -0300

    feat(app.py): changing sqlite database for a mysql database

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from models.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/flask-crud'
 
 db.init_app(app)
 login_manager.init_app(app)
