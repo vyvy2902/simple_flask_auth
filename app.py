479c6b5f51b034cab0add98e943d55654255aa2f
vyvy <vyvy292004@gmail.com>
2024-11-18 22:16:38 -0300

    feat(app.py): Configuring database

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from Flask import Flask, jsonify
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
 @app.route('/hello-world', methods=['GET'])
 def hello_world():
